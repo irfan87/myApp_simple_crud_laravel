7ba5c198cac0c55f6dddc7d9882ed9450b54e44c
irfan87 <nerve2009@yahoo.com>
2024-10-19 00:44:55 +0800

    define routes for home and products using

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <h1>Home</h1>
+    <div>
+        <p>Get your favorite item with low costs and low budget (maybe in your budget)</p>
+        <a href="{{ url('products') }}" class="btn btn-primary">Find your item!</a>
+    </div>
+</x-layout>
